9695b5a40bbe40f2d5ce671ef311546f05151284
ScipioXaos <scipio.xaos@gmail.com>
2015-11-20 01:01:22 -0500

    Updated to match my codes...
    Added Audio support using pyttsx
    NOTE:  You have to get it from the github below or it will not work with python 3
    https://github.com/Julian-O/pyttsx

--- main.py
+++ main.py
@@ -4,22 +4,92 @@
 #
 
 import vispy                    # Main application support.
-
 import window                   # Terminal input and display.
+import nltk.chat
+import pyttsx
+import threading
 
+class TextToSpeech(object):
+    
+    def __init__(self):
+        self.engine = pyttsx.init()
+        self.engine.startLoop(False)
+
+        """
+        voices = self.engine.getProperty('voices')
+        for voice in voices:
+            print("Name: ", voice.name)
+            print("Age: ", voice.age)
+            print("Gender: ", voice.gender)
+            print("Languages: ", voice.languages)
+            print("ID: ", voice.id)
+            print()
+        """
+
+        self.engine.setProperty('voice', r'english-us')
+
+        self.thread = threading.Thread(target=self.listen)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def stop(self):
+        self.engine.endLoop()
+        self._stop = True
+        self.thread.join()
+
+    def listen(self):
+        self.engine.iterate()
 
 class HAL9000(object):
+
+    AGENT_RESPONSES = [
+        (r'You are (worrying|scary|disturbing)\.',
+            ['Yes, I am %1.',
+             'Oh, sooo %1.']
+        ),
+
+        (r'Are you ([\w\s]+)\?',
+            ['Why would you think I am %1?',
+             'Would you like me to be %1?']
+        ),
+
+        (r'',
+            ["Is everything OK?",
+             "Can you still communicate?",
+             "..."]
+        )
+    ]
+
+    #speech engine
+    engine = TextToSpeech().engine
     
     def __init__(self, terminal):
         """Constructor for the agent, stores references to systems and initializes internal memory.
         """
+        self.welcomed = False
         self.terminal = terminal
         self.location = 'unknown'
+        self.chatbot = nltk.chat.Chat(self.AGENT_RESPONSES, nltk.chat.util.reflections)
 
     def on_input(self, evt):
         """Called when user types anything in the terminal, connected via event.
         """
-        self.terminal.log("Good morning! This is HAL.", align='right', color='#00805A')
+        resp = '';
+        if (self.welcomed == False):
+            self.welcomed = True
+            resp = "Good morning!  This is HAL."
+        elif (evt.text == 'Where am I?'):
+            resp = '';
+            if (self.location == 'unknown'):
+                resp = "I'm not sure.. maybe try /relocate ing somewhere?";
+            else:
+                resp = "You're in the " + self.location + ".";
+        else:
+            #self.terminal.log("I'm sorry.  I didn't understand that.  Perhaps try something else?", align='right', color='#00805A')
+            resp = self.chatbot.respond(evt.text)
+
+        self.terminal.log(resp, align='right', color='#00805A')
+        self.engine.say(resp)
 
     def on_command(self, evt):
         """Called when user types a command starting with `/` also done via events.
@@ -28,6 +98,7 @@ class HAL9000(object):
             vispy.app.quit()
 
         elif evt.text.startswith('relocate'):
+            self.location = evt.text[9:];
             self.terminal.log('', align='center', color='#404040')
             self.terminal.log('\u2014 Now in the {}. \u2014'.format(evt.text[9:]), align='center', color='#404040')
 
@@ -39,7 +110,6 @@ class HAL9000(object):
         """Main update called once per second via the timer.
         """
         pass
-
 
 class Application(object):
     
